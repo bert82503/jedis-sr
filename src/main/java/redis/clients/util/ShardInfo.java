713b290d62427ddd59d0e82317058a40c5a2f0f0
Edward Lee <bert825_work@163.com>
2014-12-02 19:51:21 +0800

    [Shard] ShardInfo<T> - "节点分片信息"抽象表示

--- src/main/java/redis/clients/util/ShardInfo.java
+++ src/main/java/redis/clients/util/ShardInfo.java
@@ -1,20 +1,55 @@
 package redis.clients.util;
 
+/**
+ * "节点分片信息"抽象表示。
+ * 
+ * @author huagang.li 2014年12月2日 下午7:37:10
+ */
 public abstract class ShardInfo<T> {
-    private int weight;
 
-    public ShardInfo() {
-    }
+	/** 默认的分片权重(虚拟节点数) */
+	private static final int DEFAULT_WEIGHT = 40;
 
-    public ShardInfo(int weight) {
-	this.weight = weight;
-    }
+	/** 分片权重 */
+	private int weight;
 
-    public int getWeight() {
-	return this.weight;
-    }
+	/**
+	 * 创建一个使用默认分片权重({@link #DEFAULT_WEIGHT})的"分片信息"实例。
+	 */
+	public ShardInfo() {
+		this.weight = DEFAULT_WEIGHT;
+	}
 
-    protected abstract T createResource();
+	/**
+	 * 创建一个使用给定分片权重的"分片信息"实例。
+	 * 
+	 * @param weight
+	 */
+	public ShardInfo(int weight) {
+		this.weight = weight;
+	}
 
-    public abstract String getName();
+	/**
+	 * 返回该节点的分片权重。
+	 * 
+	 * @return
+	 */
+	public int getWeight() {
+		return this.weight;
+	}
+
+	/**
+	 * 创建一条新的连接资源。
+	 * 
+	 * @return
+	 */
+	protected abstract T createResource();
+
+	/**
+	 * 返回节点的分片名称。
+	 * 
+	 * @return
+	 */
+	public abstract String getName();
+
 }
