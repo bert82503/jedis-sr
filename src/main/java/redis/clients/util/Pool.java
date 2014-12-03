d4f241a2464cec9069ec5d12ac176a816508fc25
Edward Lee <bert825_work@163.com>
2014-12-03 20:29:11 +0800

    [Pool] Pool<R> - 连接池抽象表示，基于GenericObjectPool<R>实现
    protected GenericObjectPool<R> internalPool：内部连接池
    
    public Pool(GenericObjectPoolConfig poolConfig, PooledObjectFactory<R>
    factory)：创建一个"连接池"实例。
    public void initPool(GenericObjectPoolConfig poolConfig,
    PooledObjectFactory<R> factory)：初始化"连接池"。
    
    public R getResource()：获取"连接池"中的一个资源。
    public void returnResource(final R resource)：将正常的资源返回给"连接池"。
    public void returnBrokenResource(final R resource)：将阻塞的资源返回给"连接池"。
    
    public boolean isClosed()：检测"连接池"是否已关闭。
    public void close()：关闭"连接池"资源。
    public void destroy()：关闭"连接池"资源，与close()方法功能一样。

--- src/main/java/redis/clients/util/Pool.java
+++ src/main/java/redis/clients/util/Pool.java
@@ -9,94 +9,160 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.clients.jedis.exceptions.JedisException;
 
-public abstract class Pool<T> implements Closeable {
-    protected GenericObjectPool<T> internalPool;
+/**
+ * 连接池抽象表示，基于{@link GenericObjectPool<T>}实现。
+ * 
+ * @author huagang.li 2014年12月3日 下午7:39:08
+ */
+public abstract class Pool<R> implements Closeable {
 
-    /**
-     * Using this constructor means you have to set and initialize the
-     * internalPool yourself.
-     */
-    public Pool() {
-    }
+	/** 内部连接池 */
+	protected GenericObjectPool<R> internalPool;
 
-    @Override
-    public void close() {
-	closeInternalPool();
-    }
-
-    public boolean isClosed() {
-	return this.internalPool.isClosed();
-    }
-
-    public Pool(final GenericObjectPoolConfig poolConfig,
-	    PooledObjectFactory<T> factory) {
-	initPool(poolConfig, factory);
-    }
-
-    public void initPool(final GenericObjectPoolConfig poolConfig,
-	    PooledObjectFactory<T> factory) {
-
-	if (this.internalPool != null) {
-	    try {
-		closeInternalPool();
-	    } catch (Exception e) {
-	    }
+	/**
+	 * Using this constructor means you have to set and initialize the
+	 * internalPool yourself.
+	 */
+	public Pool() {
 	}
 
-	this.internalPool = new GenericObjectPool<T>(factory, poolConfig);
-    }
-
-    public T getResource() {
-	try {
-	    return internalPool.borrowObject();
-	} catch (Exception e) {
-	    throw new JedisConnectionException(
-		    "Could not get a resource from the pool", e);
+	/**
+	 * 创建一个"连接池"实例。
+	 * 
+	 * @param poolConfig
+	 *            连接池配置信息
+	 * @param factory
+	 *            连接池对象的工厂
+	 */
+	public Pool(final GenericObjectPoolConfig poolConfig,
+			final PooledObjectFactory<R> factory) {
+		this.initPool(poolConfig, factory);
 	}
-    }
 
-    public void returnResourceObject(final T resource) {
-	if (resource == null) {
-	    return;
-	}
-	try {
-	    internalPool.returnObject(resource);
-	} catch (Exception e) {
-	    throw new JedisException(
-		    "Could not return the resource to the pool", e);
-	}
-    }
+	/**
+	 * 初始化"连接池"。
+	 * 
+	 * @param poolConfig
+	 *            连接池配置信息
+	 * @param factory
+	 *            连接池对象的工厂
+	 */
+	public void initPool(final GenericObjectPoolConfig poolConfig,
+			final PooledObjectFactory<R> factory) {
+		if (this.internalPool != null) {
+			try {
+				this.closeInternalPool();
+			} catch (Exception e) {
+			}
+		}
 
-    public void returnBrokenResource(final T resource) {
-	if (resource != null) {
-	    returnBrokenResourceObject(resource);
+		this.internalPool = new GenericObjectPool<R>(factory, poolConfig);
 	}
-    }
 
-    public void returnResource(final T resource) {
-	if (resource != null) {
-	    returnResourceObject(resource);
+	/**
+	 * 获取"连接池"中的一个资源。
+	 */
+	public R getResource() {
+		try {
+			return internalPool.borrowObject();
+		} catch (Exception e) {
+			// 抛出"无法从连接池中获取到一个资源"异常
+			throw new JedisConnectionException(
+					"Could not get a resource from the pool", e);
+		}
 	}
-    }
 
-    public void destroy() {
-	closeInternalPool();
-    }
-
-    protected void returnBrokenResourceObject(final T resource) {
-	try {
-	    internalPool.invalidateObject(resource);
-	} catch (Exception e) {
-	    throw new JedisException(
-		    "Could not return the resource to the pool", e);
+	/**
+	 * 将正常的资源返回给"连接池"。
+	 * 
+	 * @param resource
+	 *            正常的资源
+	 */
+	public void returnResource(final R resource) {
+		if (resource != null) {
+			this.returnResourceObject(resource);
+		}
 	}
-    }
 
-    protected void closeInternalPool() {
-	try {
-	    internalPool.close();
-	} catch (Exception e) {
-	    throw new JedisException("Could not destroy the pool", e);
+	/**
+	 * 将正常的资源返回给"连接池"。
+	 * 
+	 * @param resource
+	 */
+	protected void returnResourceObject(final R resource) {
+		if (resource == null) {
+			return;
+		}
+
+		try {
+			// 会在getTestOnReturn()时，校验资源的有效性
+			internalPool.returnObject(resource);
+		} catch (Exception e) {
+			// 抛出"无法将这个资源返回给连接池"异常
+			throw new JedisException(
+					"Could not return the resource to the pool", e);
+		}
 	}
-    }
+
+	/**
+	 * 将阻塞的资源返回给"连接池"。
+	 * 
+	 * @param resource
+	 *            阻塞的资源
+	 */
+	public void returnBrokenResource(final R resource) {
+		if (resource != null) {
+			this.returnBrokenResourceObject(resource);
+		}
+	}
+
+	/**
+	 * 将阻塞的资源返回给"连接池"。
+	 * 
+	 * @param resource
+	 */
+	protected void returnBrokenResourceObject(final R resource) {
+		try {
+			// 销毁连接池中的这个资源对象
+			internalPool.invalidateObject(resource);
+		} catch (Exception e) {
+			throw new JedisException(
+					"Could not return the resource to the pool", e);
+		}
+	}
+
+	/**
+	 * 检测"连接池"是否已关闭。
+	 */
+	public boolean isClosed() {
+		return this.internalPool.isClosed();
+	}
+
+	/**
+	 * 关闭"内部连接池"。
+	 */
+	protected void closeInternalPool() {
+		try {
+			internalPool.close();
+		} catch (Exception e) {
+			// 抛出"无法销毁该连接池"异常
+			throw new JedisException("Could not destroy the pool", e);
+		}
+	}
+
+	/**
+	 * 关闭"连接池"资源。
+	 */
+	@Override
+	public void close() {
+		this.closeInternalPool();
+	}
+
+	/**
+	 * 关闭"连接池"资源，与{@link #close()}方法功能一样。
+	 */
+	public void destroy() {
+		this.closeInternalPool();
+	}
+
 }
