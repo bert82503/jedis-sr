30dcdcc455ee659f84cfbce800e58282c12f75d4
Edward Lee <bert825_work@163.com>
2014-12-03 13:34:55 +0800

    [Client] SafeEncoder - 内容安全编码器
    public static byte[] encode(final String str)：将给定的字符串转换成字节数组。
    public static String encode(final byte[] data)：将给定的字节数组转换成字符串。

--- src/main/java/redis/clients/util/SafeEncoder.java
+++ src/main/java/redis/clients/util/SafeEncoder.java
@@ -7,35 +7,58 @@ import redis.clients.jedis.exceptions.JedisDataException;
 import redis.clients.jedis.exceptions.JedisException;
 
 /**
+ * 内容安全编码器，目的是兼容JDK 1.5。
+ * <p>
  * The only reason to have this is to be able to compatible with java 1.5 :(
- * 
  */
 public class SafeEncoder {
-    public static byte[][] encodeMany(final String... strs) {
-	byte[][] many = new byte[strs.length][];
-	for (int i = 0; i < strs.length; i++) {
-	    many[i] = encode(strs[i]);
-	}
-	return many;
-    }
 
-    public static byte[] encode(final String str) {
-	try {
-	    if (str == null) {
-		throw new JedisDataException(
-			"value sent to redis cannot be null");
-	    }
-	    return str.getBytes(Protocol.CHARSET);
-	} catch (UnsupportedEncodingException e) {
-	    throw new JedisException(e);
+	/**
+	 * 将给定的多个字符串转换成字节数组列表。
+	 * 
+	 * @param strs
+	 * @return
+	 */
+	public static byte[][] encodeMany(final String... strs) {
+		byte[][] many = new byte[strs.length][];
+		for (int i = 0; i < strs.length; i++) {
+			many[i] = encode(strs[i]);
+		}
+		return many;
 	}
-    }
 
-    public static String encode(final byte[] data) {
-	try {
-	    return new String(data, Protocol.CHARSET);
-	} catch (UnsupportedEncodingException e) {
-	    throw new JedisException(e);
+	/**
+	 * 将给定的字符串转换成字节数组。
+	 * 
+	 * @param str
+	 *            待转换的字符串
+	 * @return
+	 */
+	public static byte[] encode(final String str) {
+		try {
+			if (str == null) {
+				throw new JedisDataException(
+						"value sent to redis cannot be null");
+			}
+			return str.getBytes(Protocol.CHARSET);
+		} catch (UnsupportedEncodingException e) {
+			throw new JedisException(e);
+		}
 	}
-    }
+
+	/**
+	 * 将给定的字节数组转换成字符串。
+	 * 
+	 * @param data
+	 *            待转换的字节数组
+	 * @return
+	 */
+	public static String encode(final byte[] data) {
+		try {
+			return new String(data, Protocol.CHARSET);
+		} catch (UnsupportedEncodingException e) {
+			throw new JedisException(e);
+		}
+	}
+
 }
