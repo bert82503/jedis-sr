04ee6e0f5040a86ac2096813f1272d35ca6f2515
Edward Lee <bert825_work@163.com>
2014-12-03 14:14:03 +0800

    [Core] Connection.sendCommand(final Command cmd, final byte[]... args) - 发送一条命令到Redis服务器端
    分三步骤：
    	1. 连接到服务器；
    	2. 发送命令；
    	3. 进入管道的命令计数器加1。

--- src/main/java/redis/clients/jedis/Connection.java
+++ src/main/java/redis/clients/jedis/Connection.java
@@ -15,277 +15,326 @@ import redis.clients.util.RedisInputStream;
 import redis.clients.util.RedisOutputStream;
 import redis.clients.util.SafeEncoder;
 
+/**
+ * "Redis链接"实现。
+ * 
+ * @author huagang.li 2014年12月3日 下午1:18:04
+ */
 public class Connection implements Closeable {
-    private String host;
-    private int port = Protocol.DEFAULT_PORT;
-    private Socket socket;
-    private RedisOutputStream outputStream;
-    private RedisInputStream inputStream;
-    private int pipelinedCommands = 0;
-    private int timeout = Protocol.DEFAULT_TIMEOUT;
 
-    private boolean broken = false;
+	/** 主机域名/IP */
+	private String host;
+	/** 端口号 */
+	private int port = Protocol.DEFAULT_PORT;
 
-    public Socket getSocket() {
-	return socket;
-    }
+	/** 链接套接字 */
+	private Socket socket;
+	/** 输出流 */
+	private RedisOutputStream outputStream;
+	/** 输入流 */
+	private RedisInputStream inputStream;
+	/** 套接字存活保持时间 */
+	private int timeout = Protocol.DEFAULT_TIMEOUT;
+	/** 链接是否阻塞了 */
+	private boolean broken = false;
+	/** 已进入管道的命令计数器 */
+	private int pipelinedCommands = 0;
 
-    public int getTimeout() {
-	return timeout;
-    }
-
-    public void setTimeout(final int timeout) {
-	this.timeout = timeout;
-    }
-
-    public void setTimeoutInfinite() {
-	try {
-	    if (!isConnected()) {
-		connect();
-	    }
-	    socket.setKeepAlive(true);
-	    socket.setSoTimeout(0);
-	} catch (SocketException ex) {
-	    broken = true;
-	    throw new JedisConnectionException(ex);
+	/**
+	 * 创建一条新的Redis链接。
+	 * 
+	 * @param host
+	 */
+	public Connection(final String host) {
+		this.host = host;
 	}
-    }
 
-    public void rollbackTimeout() {
-	try {
-	    socket.setSoTimeout(timeout);
-	    socket.setKeepAlive(false);
-	} catch (SocketException ex) {
-	    broken = true;
-	    throw new JedisConnectionException(ex);
+	/**
+	 * 返回链接套接字。
+	 */
+	public Socket getSocket() {
+		return socket;
 	}
-    }
 
-    public Connection(final String host) {
-	super();
-	this.host = host;
-    }
-
-    protected Connection sendCommand(final Command cmd, final String... args) {
-	final byte[][] bargs = new byte[args.length][];
-	for (int i = 0; i < args.length; i++) {
-	    bargs[i] = SafeEncoder.encode(args[i]);
+	public int getTimeout() {
+		return timeout;
 	}
-	return sendCommand(cmd, bargs);
-    }
 
-    protected Connection sendCommand(final Command cmd, final byte[]... args) {
-	try {
-	    connect();
-	    Protocol.sendCommand(outputStream, cmd, args);
-	    pipelinedCommands++;
-	    return this;
-	} catch (JedisConnectionException ex) {
-	    // Any other exceptions related to connection?
-	    broken = true;
-	    throw ex;
+	public void setTimeout(final int timeout) {
+		this.timeout = timeout;
 	}
-    }
 
-    protected Connection sendCommand(final Command cmd) {
-	try {
-	    connect();
-	    Protocol.sendCommand(outputStream, cmd, new byte[0][]);
-	    pipelinedCommands++;
-	    return this;
-	} catch (JedisConnectionException ex) {
-	    // Any other exceptions related to connection?
-	    broken = true;
-	    throw ex;
-	}
-    }
-
-    public Connection(final String host, final int port) {
-	super();
-	this.host = host;
-	this.port = port;
-    }
-
-    public String getHost() {
-	return host;
-    }
-
-    public void setHost(final String host) {
-	this.host = host;
-    }
-
-    public int getPort() {
-	return port;
-    }
-
-    public void setPort(final int port) {
-	this.port = port;
-    }
-
-    public Connection() {
-
-    }
-
-    public void connect() {
-	if (!isConnected()) {
-	    try {
-		socket = new Socket();
-		// ->@wjw_add
-		socket.setReuseAddress(true);
-		socket.setKeepAlive(true); // Will monitor the TCP connection is
-					   // valid
-		socket.setTcpNoDelay(true); // Socket buffer Whetherclosed, to
-					    // ensure timely delivery of data
-		socket.setSoLinger(true, 0); // Control calls close () method,
-					     // the underlying socket is closed
-					     // immediately
-		// <-@wjw_add
-
-		socket.connect(new InetSocketAddress(host, port), timeout);
-		socket.setSoTimeout(timeout);
-		outputStream = new RedisOutputStream(socket.getOutputStream());
-		inputStream = new RedisInputStream(socket.getInputStream());
-	    } catch (IOException ex) {
-		broken = true;
-		throw new JedisConnectionException(ex);
-	    }
-	}
-    }
-
-    @Override
-    public void close() {
-	disconnect();
-    }
-
-    public void disconnect() {
-	if (isConnected()) {
-	    try {
-		inputStream.close();
-		outputStream.close();
-		if (!socket.isClosed()) {
-		    socket.close();
+	/**
+	 * 设置链接不超时。
+	 */
+	public void setTimeoutInfinite() {
+		try {
+			if (!isConnected()) {
+				connect();
+			}
+			socket.setKeepAlive(true);
+			socket.setSoTimeout(0); // 0：表示链接永不超时
+		} catch (SocketException ex) {
+			broken = true;
+			throw new JedisConnectionException(ex);
 		}
-	    } catch (IOException ex) {
-		broken = true;
-		throw new JedisConnectionException(ex);
-	    }
 	}
-    }
 
-    public boolean isConnected() {
-	return socket != null && socket.isBound() && !socket.isClosed()
-		&& socket.isConnected() && !socket.isInputShutdown()
-		&& !socket.isOutputShutdown();
-    }
-
-    protected String getStatusCodeReply() {
-	flush();
-	pipelinedCommands--;
-	final byte[] resp = (byte[]) readProtocolWithCheckingBroken();
-	if (null == resp) {
-	    return null;
-	} else {
-	    return SafeEncoder.encode(resp);
+	public void rollbackTimeout() {
+		try {
+			socket.setSoTimeout(timeout);
+			socket.setKeepAlive(false);
+		} catch (SocketException ex) {
+			broken = true;
+			throw new JedisConnectionException(ex);
+		}
 	}
-    }
 
-    public String getBulkReply() {
-	final byte[] result = getBinaryBulkReply();
-	if (null != result) {
-	    return SafeEncoder.encode(result);
-	} else {
-	    return null;
+	/**
+	 * 发送一条命令到Redis服务器端。
+	 * 
+	 * @param cmd
+	 *            Redis命令
+	 * @param args
+	 *            参数列表
+	 * @return
+	 */
+	protected Connection sendCommand(final Command cmd, final String... args) {
+		final byte[][] bargs = new byte[args.length][];
+		for (int i = 0; i < args.length; i++) {
+			bargs[i] = SafeEncoder.encode(args[i]);
+		}
+		return sendCommand(cmd, bargs);
 	}
-    }
 
-    public byte[] getBinaryBulkReply() {
-	flush();
-	pipelinedCommands--;
-	return (byte[]) readProtocolWithCheckingBroken();
-    }
-
-    public Long getIntegerReply() {
-	flush();
-	pipelinedCommands--;
-	return (Long) readProtocolWithCheckingBroken();
-    }
-
-    public List<String> getMultiBulkReply() {
-	return BuilderFactory.STRING_LIST.build(getBinaryMultiBulkReply());
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<byte[]> getBinaryMultiBulkReply() {
-	flush();
-	pipelinedCommands--;
-	return (List<byte[]>) readProtocolWithCheckingBroken();
-    }
-
-    public void resetPipelinedCount() {
-	pipelinedCommands = 0;
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<Object> getRawObjectMultiBulkReply() {
-	return (List<Object>) readProtocolWithCheckingBroken();
-    }
-
-    public List<Object> getObjectMultiBulkReply() {
-	flush();
-	pipelinedCommands--;
-	return getRawObjectMultiBulkReply();
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<Long> getIntegerMultiBulkReply() {
-	flush();
-	pipelinedCommands--;
-	return (List<Long>) readProtocolWithCheckingBroken();
-    }
-
-    public List<Object> getAll() {
-	return getAll(0);
-    }
-
-    public List<Object> getAll(int except) {
-	List<Object> all = new ArrayList<Object>();
-	flush();
-	while (pipelinedCommands > except) {
-	    try {
-		all.add(readProtocolWithCheckingBroken());
-	    } catch (JedisDataException e) {
-		all.add(e);
-	    }
-	    pipelinedCommands--;
+	/**
+	 * 发送一条命令到Redis服务器端。
+	 * 
+	 * <pre>
+	 * 分三步骤：
+	 * 	1. 连接到服务器；
+	 * 	2. 发送命令；
+	 * 	3. 进入管道的命令计数器加1。
+	 * </pre>
+	 * 
+	 * @param cmd
+	 *            Redis命令
+	 * @param args
+	 *            参数列表
+	 * @return
+	 */
+	protected Connection sendCommand(final Command cmd, final byte[]... args) {
+		try {
+			connect();
+			Protocol.sendCommand(outputStream, cmd, args);
+			pipelinedCommands++;
+			return this;
+		} catch (JedisConnectionException ex) {
+			// Any other exceptions related to connection?
+			broken = true;
+			throw ex;
+		}
 	}
-	return all;
-    }
 
-    public Object getOne() {
-	flush();
-	pipelinedCommands--;
-	return readProtocolWithCheckingBroken();
-    }
-
-    public boolean isBroken() {
-	return broken;
-    }
-
-    protected void flush() {
-	try {
-	    outputStream.flush();
-	} catch (IOException ex) {
-	    broken = true;
-	    throw new JedisConnectionException(ex);
+	protected Connection sendCommand(final Command cmd) {
+		try {
+			connect();
+			Protocol.sendCommand(outputStream, cmd, new byte[0][]);
+			pipelinedCommands++;
+			return this;
+		} catch (JedisConnectionException ex) {
+			// Any other exceptions related to connection?
+			broken = true;
+			throw ex;
+		}
 	}
-    }
 
-    protected Object readProtocolWithCheckingBroken() {
-	try {
-	    return Protocol.read(inputStream);
-	} catch (JedisConnectionException exc) {
-	    broken = true;
-	    throw exc;
+	public Connection(final String host, final int port) {
+		super();
+		this.host = host;
+		this.port = port;
 	}
-    }
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(final String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(final int port) {
+		this.port = port;
+	}
+
+	public Connection() {
+
+	}
+
+	public void connect() {
+		if (!isConnected()) {
+			try {
+				socket = new Socket();
+				// ->@wjw_add
+				socket.setReuseAddress(true);
+				socket.setKeepAlive(true); // Will monitor the TCP connection is
+				// valid
+				socket.setTcpNoDelay(true); // Socket buffer Whetherclosed, to
+				// ensure timely delivery of data
+				socket.setSoLinger(true, 0); // Control calls close () method,
+				// the underlying socket is closed
+				// immediately
+				// <-@wjw_add
+
+				socket.connect(new InetSocketAddress(host, port), timeout);
+				socket.setSoTimeout(timeout);
+				outputStream = new RedisOutputStream(socket.getOutputStream());
+				inputStream = new RedisInputStream(socket.getInputStream());
+			} catch (IOException ex) {
+				broken = true;
+				throw new JedisConnectionException(ex);
+			}
+		}
+	}
+
+	@Override
+	public void close() {
+		disconnect();
+	}
+
+	public void disconnect() {
+		if (isConnected()) {
+			try {
+				inputStream.close();
+				outputStream.close();
+				if (!socket.isClosed()) {
+					socket.close();
+				}
+			} catch (IOException ex) {
+				broken = true;
+				throw new JedisConnectionException(ex);
+			}
+		}
+	}
+
+	public boolean isConnected() {
+		return socket != null && socket.isBound() && !socket.isClosed()
+				&& socket.isConnected() && !socket.isInputShutdown()
+				&& !socket.isOutputShutdown();
+	}
+
+	protected String getStatusCodeReply() {
+		flush();
+		pipelinedCommands--;
+		final byte[] resp = (byte[]) readProtocolWithCheckingBroken();
+		if (null == resp) {
+			return null;
+		} else {
+			return SafeEncoder.encode(resp);
+		}
+	}
+
+	public String getBulkReply() {
+		final byte[] result = getBinaryBulkReply();
+		if (null != result) {
+			return SafeEncoder.encode(result);
+		} else {
+			return null;
+		}
+	}
+
+	public byte[] getBinaryBulkReply() {
+		flush();
+		pipelinedCommands--;
+		return (byte[]) readProtocolWithCheckingBroken();
+	}
+
+	public Long getIntegerReply() {
+		flush();
+		pipelinedCommands--;
+		return (Long) readProtocolWithCheckingBroken();
+	}
+
+	public List<String> getMultiBulkReply() {
+		return BuilderFactory.STRING_LIST.build(getBinaryMultiBulkReply());
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<byte[]> getBinaryMultiBulkReply() {
+		flush();
+		pipelinedCommands--;
+		return (List<byte[]>) readProtocolWithCheckingBroken();
+	}
+
+	public void resetPipelinedCount() {
+		pipelinedCommands = 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Object> getRawObjectMultiBulkReply() {
+		return (List<Object>) readProtocolWithCheckingBroken();
+	}
+
+	public List<Object> getObjectMultiBulkReply() {
+		flush();
+		pipelinedCommands--;
+		return getRawObjectMultiBulkReply();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Long> getIntegerMultiBulkReply() {
+		flush();
+		pipelinedCommands--;
+		return (List<Long>) readProtocolWithCheckingBroken();
+	}
+
+	public List<Object> getAll() {
+		return getAll(0);
+	}
+
+	public List<Object> getAll(int except) {
+		List<Object> all = new ArrayList<Object>();
+		flush();
+		while (pipelinedCommands > except) {
+			try {
+				all.add(readProtocolWithCheckingBroken());
+			} catch (JedisDataException e) {
+				all.add(e);
+			}
+			pipelinedCommands--;
+		}
+		return all;
+	}
+
+	public Object getOne() {
+		flush();
+		pipelinedCommands--;
+		return readProtocolWithCheckingBroken();
+	}
+
+	public boolean isBroken() {
+		return broken;
+	}
+
+	protected void flush() {
+		try {
+			outputStream.flush();
+		} catch (IOException ex) {
+			broken = true;
+			throw new JedisConnectionException(ex);
+		}
+	}
+
+	protected Object readProtocolWithCheckingBroken() {
+		try {
+			return Protocol.read(inputStream);
+		} catch (JedisConnectionException exc) {
+			broken = true;
+			throw exc;
+		}
+	}
 }
